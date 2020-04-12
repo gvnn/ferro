ab1018eb8880688f6311451d78125b1368540d26
Giovanni Ferron <gvnnrulez@gmail.com>
2020-04-12 11:33:14 +1000

    Couple of lines of code to test actix-web

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+//! Actix web juniper example
+//!
+//! A simple example integrating juniper in actix-web
+use std::io;
+use std::sync::Arc;
+
+use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer, Result};
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+use askama::Template;
+
+mod schema;
+
+use crate::schema::{create_schema, Schema};
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct Hello<'a> {
+    name: &'a str,
+}
+
+async fn graphiql() -> HttpResponse {
+    let html = graphiql_source("http://127.0.0.1:8080/graphql");
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
+}
+
+async fn graphql(
+    st: web::Data<Arc<Schema>>,
+    data: web::Json<GraphQLRequest>,
+) -> Result<HttpResponse, Error> {
+    let user = web::block(move || {
+        let res = data.execute(&st, &());
+        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+    })
+        .await?;
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(user))
+}
+
+async fn index() -> Result<HttpResponse> {
+    let s = Hello { name: "gvnn" }.render().unwrap();
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    // Create Juniper schema
+    let schema = std::sync::Arc::new(create_schema());
+
+    // Start http server
+    HttpServer::new(move || {
+        App::new()
+            .data(schema.clone())
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/").route(web::get().to(index)))
+            .service(web::resource("/graphql").route(web::post().to(graphql)))
+            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
